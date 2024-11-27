555efb124ecf6dbe765c5ab3c7447c0f3803f693
Fejiro001 <aberefejiro@gmail.com>
2024-11-27 09:45:37 +0100

    new action methods added to User controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::with('folders')->get();
+        return response()->json($users);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email|unique:users,email'
+        ]);
+
+        $user = User::create([
+            'email' => $validated['email']
+        ]);
+
+        return response()->json([
+            'message' => 'User added successfully',
+            'user' => $user
+        ]);
+    }
 }
