b71923201319fc3cc2f58bf1992d6c8bb740fed5
Fejiro001 <aberefejiro@gmail.com>
2024-11-27 09:38:57 +0100

    SSO using Laravel Socialite

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,41 +2,76 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Auth;
-use Illuminate\Http\Request;
+use App\Models\User;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
 {
-    public function redirectToProvider($provider)
+    private const PROVIDER = 'google';
+    private const TOKEN_NAME = 'Google';
+
+    public function redirectToProvider()
     {
-        return Socialite::driver($provider)->stateless()->redirect();
+        return Socialite::driver(self::PROVIDER)
+            ->scopes(['openid', 'profile', 'email'])->with(['access_type' => 'offline'])
+            ->stateless()
+            ->redirect();
     }
 
-    public function handleProviderCallback($provider)
+    public function handleProviderCallback()
     {
         try {
-            // Gets user from the provider
-            $user = Socialite::driver($provider)->stateless()->user();
+            $googleUser = Socialite::driver(self::PROVIDER)->stateless()->user();
 
-            $authUser = User::firstOrCreate([
-                'email' => $user->getEmail(),
+            \Log::info('Google User:', (array) $googleUser);
+
+            $user = User::updateOrCreate([
+                'provider' => self::PROVIDER,
+                'email' => $googleUser->getEmail(),
+                'provider_id' => $googleUser->getId(),
             ], [
-                'name' => $user->getName(),
-                'avatar' => $user->getAvatar(),
-                'provider' => $provider,
-                'provider_id' => $user->getId(),
-                'token' => $user->token,
-                'refreshToken' => $user->refreshToken,
+                'name' => $googleUser->getName(),
+                'token' => $googleUser->token,
+                'refresh_token' => $googleUser->refreshToken,
             ]);
 
-            Auth::login($authUser);
+            Auth::login($user, true);
 
-            return redirect()->to('/');
-        } catch (\Exception $exception) {
-            \Log::error("Google login error: " . $exception->getMessage());
+            $token = $user->createToken('authToken')->accessToken;
+
+            \Log::info('Generated token: ' . $token);
+
+            $cookie = cookie('authToken', $token, 60 * 24, null, 'localhost', false, true, false, 'None');
+
+            return redirect('http://localhost:5173')->withCookie($cookie);
+        } catch (\Exception $e) {
+            \Log::error('Google login failed: ' . $e->getMessage());
+            return response()->json(['error' => 'Failed to login with Google.'], 400);
+        }
+    }
+
+    public function getUserName()
+    {
+        $user = Auth::user();
+        \Log::info("User info: " . json_encode($user)); 
+        return response()->json([
+            'name' => $user->name,
+        ]);
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+
+        if ($user) {
+            $user->token()->revoke();
+            Auth::logout();
+
+            return response()->json(['message' => 'Successfully logged out']);
+
         }
 
+        return response()->json(['message' => 'No user logged in'], 400);
     }
 }
