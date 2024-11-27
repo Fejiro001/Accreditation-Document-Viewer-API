78bc4b471de0d35579c42191522ab3bac5c9a26b
Fejiro001 <aberefejiro@gmail.com>
2024-11-27 09:46:04 +0100

    removed Microsoft provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
-            $event->extendSocialite('microsoft', \SocialiteProviders\Microsoft\Provider::class);
-        });
-        Passport::hashClientSecrets();
+        // Passport::hashClientSecrets();
     }
 }
