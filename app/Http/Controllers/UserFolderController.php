44911d2321c34debf063214e64a64af48c570511
Fejiro001 <aberefejiro@gmail.com>
2024-11-29 16:07:09 +0100

    controller to manage user folder relationship

--- app/Http/Controllers/UserFolderController.php
+++ app/Http/Controllers/UserFolderController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserFolderController extends Controller
 {
-    //
+    public function store(Request $request, User $user)
+    {
+        $validated = $request->validate([
+            'folder_id' => 'required|string'
+        ]);
+
+        $user->folders()->syncWithoutDetaching([$validated['folder_id']]);
+
+        return response()->json(['message' => 'Folder access granted!']);
+    }
+
+    public function destroy(User $user, $folder_id)
+    {
+        $user->folders()->detach($folder_id);
+
+        return response()->json(['message' => 'Folder access revoked!']);
+    }
 }
