941feb09cfec9d4720816ec185ae55bd053645bc
Fejiro001 <aberefejiro@gmail.com>
2024-12-09 14:27:19 +0100

    removed unneeded method and fillable

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -14,7 +14,6 @@ class Folder extends Model
     protected $fillable = [
         'name',
         'google_drive_id',
-        'parent_folder_id'
     ];
 
     /**
@@ -25,25 +24,5 @@ class Folder extends Model
     public function users()
     {
         return $this->belongsToMany(User::class, 'folder_user', 'folder_id', 'user_id')->withTimestamps();
-    }
-
-    /**
-     * Define the relationship with itself to manage folder hierarchy.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function children()
-    {
-        return $this->hasMany(Folder::class, 'parent_folder_id');
-    }
-
-    /**
-     * Define the relationship to access the parent folder.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function parent()
-    {
-        return $this->belongsTo(Folder::class, 'parent_folder_id');
     }
 }
